e1268dec584fa8f0cd14c530ee0c053d15f56366
⚡ CT_PRO ⚡ <89440790+Ctzfamily@users.noreply.github.com>
2021-11-19 15:29:07 +0530

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -681,7 +681,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@ZeusSupportChat","**Zesu** Back Online (:", parse_mode=ParseMode.MARKDOWN) 
+            dispatcher.bot.sendMessage(f"@ZeusSupportChat","[Zeus Back Online (:](https://telegra.ph/file/44e69dbd43565104662f3.jpg)", parse_mode=ParseMode.MARKDOWN) 
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!",
