5428a1b86de1bb71ed436bd185b5f3752527adad
zero <94368175+zerosquad13@users.noreply.github.com>
2021-11-30 02:50:42 +0700

    Update tagall.py

--- VegetaRobot/modules/tagall.py
+++ VegetaRobot/modules/tagall.py
@@ -55,6 +55,19 @@ async def mentionall(event):
         await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
+
+# Cancle 
+
+@telethn.on(events.NewMessage(pattern="^/cancel?(.*)"))
+async def cancel_spam(event):
+  if not event.chat_id in spam_chats:
+    return await event.respond('__There is no proccess on going...__')
+  else:
+    try:
+      spam_chats.remove(event.chat_id)
+    except:
+      pass
+    return await event.respond('**__Stoped__**\n\n**__Powered By:__ @lifeinsinn**')
         
 
         
