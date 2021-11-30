7db84887430e4ec01d290f7664c3352b1cf2f133
zero <94368175+zerosquad13@users.noreply.github.com>
2021-12-01 04:08:55 +0700

    Update animation.py

--- VegetaRobot/modules/animation.py
+++ VegetaRobot/modules/animation.py
@@ -4,9 +4,9 @@ from typing import List
 from telegram import Update
 from telegram.ext import run_async, CallbackContext
 
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin
+from VegetaRobot import dispatcher
+from VegetaRobot.modules.disable import DisableAbleCommandHandler
+from VegetaRobot.modules.helper_funcs.chat_status import user_admin
 
 # sleep how many times after each edit in 'love'
 EDIT_SLEEP = 1
